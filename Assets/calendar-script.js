9286509dcf2c2a69cd81e2f2089e4d69187edd08
r0901578 <r0901578@student.thomasmore.be>
2022-04-26 21:30:59 +0200

    Update Calendar

--- Assets/calendar-script.js
+++ Assets/calendar-script.js
@@ -1,1 +1,23 @@
-console.log("Hello")
+var date = new Date();
+
+// Setting up months
+
+var months = 
+[
+   "January",
+   "February", 
+   "March", 
+   "April", 
+   "May", 
+   "June", 
+   "July", 
+   "August", 
+   "September", 
+   "October", 
+   "November", 
+   "December", 
+]
+
+// To change the month
+document.querySelector(".month-name2").innerHTML
+=months[1];

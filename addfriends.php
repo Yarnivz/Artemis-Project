bfa3c732d589cd51b93076655416cf0a5144abc9
Shafiqgit1 <Shafiqgit1>
2022-04-29 14:21:42 +0200

    added settings and search

---
+++ addfriends.php
@@ -0,0 +1,13 @@
+<?php
+
+$connection = new mysqli('localhost', 'root', 'password', 'DB_NAME');
+
+$search = $_GET['search'];
+$search = $mysqli -> real_escape_string($search);
+
+$query = "SELECT username FROM member WHERE username LIKE '%".$search."%'";
+$result= $mysqli -> query($query);
+
+while($row = $result -> fetch_object()){
+    echo "<div id='link' onClick='addText(\"".$row -> username."\");'>" . $row -> username . "</div>";  
+}

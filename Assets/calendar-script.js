562b7995d326ec90a0783a36103299d403b59814
r0901578 <r0901578@student.thomasmore.be>
2022-04-30 13:26:58 +0200

    Update Calendar

--- Assets/calendar-script.js
+++ Assets/calendar-script.js
@@ -2,9 +2,10 @@
 var date = new Date();
 var currentMonth = date.getMonth();
 var currentYear = date.getFullYear();
+var currentDay = date.getDate();
+var daysOfMonth = document.querySelector(".days");
 
 // Setting up months   
-
 var months = 
 [
    "January",
@@ -19,24 +20,41 @@ var months =
    "October", 
    "November", 
    "December", 
-]
+];
 
-// To change the month
+//Setting The Date into the current month
 document.querySelector(".month-name2").innerHTML = months[currentMonth];
-
 document.querySelector(".year-name").innerHTML = currentYear;
 
 
-//button element for switching the date
-var previousButton = document.querySelector(".previous-button");
-var nextButton = document.querySelector(".next-button");
+//To have each day in the calendar
+var days = "";
+var lastDay = new Date(currentYear, currentMonth + 1 , 0).getDate();
+var previousLastDays = new Date(currentYear, currentMonth,0).getDate();
 
-previousButton.addEventListener("click",function(){
-   date.setMonth(currentMonth - 1);
-   calendarFunction();
-});
+//The days of previous month
+var firstDayIndex = date.getDay();
 
-nextButton.addEventListener("click",function(){
-   date.setMonth(currentMonth + 1);
-   calendarFunction();
-});
+for(var x = firstDayIndex; x > 0; x++)
+{
+   var previousDays = '<li class="previous-days">' + (previousLastDays - x + 1) + '</li>';
+   days += previousDays;
+}
+
+//The days of the specific month
+for(var i = 1; i <= lastDay; i++)
+{
+   var usageDays = '<li>' + i +'</li>';
+   days += usageDays;
+}
+daysOfMonth.innerHTML = days;
+
+//the days of next month
+var lastDayIndex = new Date(currentYear, currentMonth + 1, 0).getDay();
+var nextDays = 7 - lastDayIndex
+
+for(var j = 1; j <= nextDays; j++)
+{
+   var usageDays = '<li class="next-days">' + j +'</li>';
+   days += usageDays;
+}

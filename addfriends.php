338bd188166eabf02a9538775001a1276eb962d4
r0901578 <r0901578@student.thomasmore.be>
2022-05-07 16:13:48 +0200

    Update

--- addfriends.php
+++ addfriends.php
@@ -2,16 +2,31 @@
 
     include "Assets/database/connection.php";
 
-    $connection = new mysqli('localhost', 'root', 'password', 'DB_NAME');
+    $conn = dbConnection();
+    session_start();
+    $firstname = $_POST["firstname"];
+    $lastname = $_POST["lastname"];
+    $gender = $_POST["gender"];
+    $birthdate = $_POST["birthdate"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $addfriend = $_POST["add friend"];
 
-    $search = $_GET['search'];
-    $search = $mysqli -> real_escape_string($search);
 
-    $query = "SELECT username FROM member WHERE username LIKE '%".$search."%'";
-    $result= $mysqli -> query($query);
 
-    while($row = $result -> fetch_object()){
-        echo "<div id='link' onClick='addText(\"".$row -> username."\");'>" . $row -> username . "</div>";  
-    }
+
+
+
+    // $connection = new mysqli('localhost', 'root', 'password', 'DB_NAME');
+
+    // $search = $_GET['search'];
+    // $search = $mysqli -> real_escape_string($search);
+
+    // $query = "SELECT username FROM member WHERE username LIKE '%".$search."%'";
+    // $result= $mysqli -> query($query);
+
+    // while($row = $result -> fetch_object()){
+    //     echo "<div id='link' onClick='addText(\"".$row -> username."\");'>" . $row -> username . "</div>";  
+    // }
 
 ?>

45124b47b60eb916e6667c4f540230af0b50f37b
Mihaela Fetcu <mihaelafetcu@Mihaelas-MacBook-Air.local>
2022-05-24 16:16:46 +0200

    editted

--- assets/database/database.sql
+++ assets/database/database.sql
@@ -79,8 +79,8 @@ FOREIGN KEY (UserID, GuestID) REFERENCES Guests(UserID, GuestID)
 CREATE TABLE Gifts (
 `GiftID` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `ProductName` VARCHAR(50) NOT NULL,
-`Price` DOUBLE NOT NULL
-
+`Price` DOUBLE NOT NULL,
+`ImageURL` VARCHAR(1000),
 `Website` VARCHAR(1000),
 `Preference` INT,
 FOREIGN KEY ( Preference) REFERENCES Preferences( PreferenceID)

a8b39c2760abcb77cf0f94fdbfc8516501a95e66
Shafiqgit1 <Shafiqgit1>
2022-05-25 12:18:16 +0200

    added finals for calendar and shopping

--- calendar.php
+++ calendar.php
@@ -1,7 +1,27 @@
 <?php
   include "functions.php";
   authenticate();
+  include "Assets/database/connection.php";
+    $conn = dbConnection(); 
+    session_start();
+    $userID = $_SESSION["loggedUser"]["UserId"];
+    
+$query1 = "SELECT * FROM User_Preferences WHERE UserID=$userID ";
+$queryResult1 = mysqli_query($conn, $query1);
+
+$returnRow1 = $queryResult1 -> fetch_all(MYSQLI_ASSOC);
+$row=0;
+foreach($returnRow1 as $value){
+    $preferenceID[$row] = $value["PreferenceID"]; 
+    $row=$row+1;
+}
+
+$query = "SELECT * FROM Gifts WHERE Preference=$preferenceID[0] OR Preference=$preferenceID[1] OR Preference=$preferenceID[2] ORDER BY RAND() LIMIT 5 ";
+
+$queryResult = mysqli_query($conn, $query);
+$returnRow = $queryResult -> fetch_all(MYSQLI_ASSOC);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -84,9 +104,12 @@
             <!-- Usage submit & close form -->
             
             <tr>
-              <td><input type="submit" id="addEventBTN" value="Add Event"></td>
-              <td><button id="closeBTN" onclick="closeForm()" form="noForm">Close Window</button></td>
+              <td><button class="hero-btn1" type="submit" id="addEventBTN" value="Add Event">Add </button>
+              <td><button class="hero-btn1" id="closeBTN" onclick="closeForm()" form="noForm">Close</button>
             </tr>
+            
+               
+
 
           </table>
         
@@ -94,6 +117,46 @@
         
       </fieldset>
     </div>  
+    <section class="header2">
+
+    <div class="shopping">
+            <?php
+                echo( "<button onclick= \"location.href='calendar.php'\">Refresh</button>") . "<br>";
+
+                foreach($returnRow as $value){
+                    $productName = $value["ProductName"];
+                    $price = $value["Price"];
+                    $websitelink = $value["Website"]; ?>
+                    <h3 class="shoplist">
+                       <?php echo  $productName . "<br>"; ?>
+                    </h3> 
+                   <h3 class="shoplist">
+                        <?php echo "Price: ". $price ."$". "<br>"; ?>
+                   </h3>
+                    <div class="Portrait">
+                    <?php
+        
+                    
+                    $imageURL = $value["ImageURL"];
+                    $imageData = base64_encode(file_get_contents($imageURL));
+                    
+                  
+                        
+
+                         echo '<img src="data:image/jpeg;base64,'.$imageData.'">' . "<br>"; 
+                         
+                         echo( "<button onclick= \"location.href='$websitelink'\">Buy</button>") . "<br>"; ?>
+                    </div>
+                   
+                 <?php   
+                }
+                ?>
+
+            </div>
+    
+
+    </section>
+   
     
     
     <!-- Library tool for the calendar  -->
